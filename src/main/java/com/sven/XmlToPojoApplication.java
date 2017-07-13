ce6bddd8f567018690a4bbb13bf1c4d65832c44e
kwan@bmj.com <KWan@BMJ-DT14466.bmauk.net>
2017-07-13 16:39:01 +0100

    init commit

---
+++ src/main/java/com/sven/XmlToPojoApplication.java
@@ -0,0 +1,12 @@
+package com.sven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XmlToPojoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XmlToPojoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/sven/XmlToPojoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sven;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class XmlToPojoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
